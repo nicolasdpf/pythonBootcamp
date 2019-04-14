bbfdfcce27a95f8c2edb178732a7c0324c4066c9
danntastico <nicolasdpf@gmail.com>
2019-04-13 20:36:08 -0500

    hgjhgj

---
+++ Python-BootCamp/Lists.py
@@ -0,0 +1,16 @@
+toyota= ["Camry", "Corlla", "Nissan"]
+del toyota[2]
+print(toyota)
+
+
+#Para ordenar 
+List = ["Hola", "Amanda", "toma"]
+List.sort()
+
+#Para ordenar en sentido contrario
+List.sort(reverse = True)
+print(List)
+
+#Para imprimir el tamaño de la lista
+
+print(len(List))
